dda85096dc12caf7fb2954394dcd5d29f1721a2a
Darguima <dsgdevbraga@gmail.com>
2025-01-24 17:28:43 +0000

    more modal stuff

--- Data/Models/Product.cs
+++ Data/Models/Product.cs
@@ -4,7 +4,7 @@ public record Product
 {
     public required int Model { get; set; }
     public required string Name { get; set; }
-    public required int Price { get; set; }
+    public required decimal Price { get; set; }
     public required string Description { get; set; }
     public required string Image { get; set; }
 }
